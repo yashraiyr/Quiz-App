9d7227c98b85e8b020903fa06dfa4c7cd8e11dd8
yashraiyr <yashfame.raiyr@gmail.com>
2022-06-30 02:15:13 +0530

    Updates

---
+++ precache-manifest.28f9044e78b399d37a7cb16dd415b649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae9152bb3a841dc6af3",
+    "url": "/Quiz-App/static/css/main.d4efc68a.chunk.css"
+  },
+  {
+    "revision": "3ae9152bb3a841dc6af3",
+    "url": "/Quiz-App/static/js/main.3ae9152b.chunk.js"
+  },
+  {
+    "revision": "73d3c09f02479628f9ef",
+    "url": "/Quiz-App/static/js/1.73d3c09f.chunk.js"
+  },
+  {
+    "revision": "83ce9d8c01920f24445e",
+    "url": "/Quiz-App/static/js/runtime~main.83ce9d8c.js"
+  },
+  {
+    "revision": "0cd0aceda704d1d48984514712e923f4",
+    "url": "/Quiz-App/index.html"
+  }
+];
